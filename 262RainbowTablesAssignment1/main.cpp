7c688f69bff2eb184e65741d7ef06c57e4c88bc3
Fawaz Siddiqi <mfawazsiddiqi@gmail.com>
2018-11-03 18:20:59 +0400

    Initial Commit

---
+++ 262RainbowTablesAssignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  262RainbowTablesAssignment1
+//
+//  Created by Fawaz Siddiqi on 11/3/18.
+//  Copyright © 2018 Fawaz Siddiqi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
